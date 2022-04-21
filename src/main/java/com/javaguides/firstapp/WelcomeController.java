28a67f08c5451c556fe1d2db371e86a384d239a7
Nayanalekha <38463625+Nayanalekha@users.noreply.github.com>
2022-04-21 18:04:31 +1200

    Initial commit

---
+++ src/main/java/com/javaguides/firstapp/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.javaguides.firstapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return " Welcome to Sprint boot world";
+    }
+}
